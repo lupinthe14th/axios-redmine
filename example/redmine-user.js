9a8e700fa1a50be6ea26f747e92e8bc29f6e4f07
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-10-13 23:55:59 +0900

    [modify] enable to work

--- example/redmine-user.js
+++ example/redmine-user.js
@@ -1,54 +1,59 @@
 /*
  * get-redmine-issue - test request for issues
- * Author: wayne <wayne@zanran.me>
+ * Author: lupinthe14th <hideosuzuki@ordinarius-fectum.net>
  */
 
-'use strict()';
+'use strict()'
 
-var Redmine = require('../lib/redmine');
+const Redmine = require('../lib/redmine')
 
-///////////////////////////////////////////////////////////////
-var hostname = process.env.REDMINE_HOST || 'redmine.zanran.me';
-var config = {
-  apiKey: process.env.REDMINE_APIKEY || 'bed1ba0544b681e530c2447341607f423c9c8781',
-  format: 'json'
-};
+/// ////////////////////////////////////////////////////////////
+const hostname =
+  process.env.REDMINE_HOST || 'https://docker.for.mac.host.internal'
+const config = {
+  apiKey:
+    process.env.REDMINE_APIKEY || 'b7ce4d8d3865e79a75da8dba39bc801c12e36488',
+  rejectUnauthorized: process.env.REGECT_UNAUTHORIZED
+}
 
-var redmine = new Redmine(hostname, config);
+const redmine = new Redmine(hostname, config)
 
-
-var dump_user = function(user) {
-  console.log('Dumping user:');
-  for (var item in user) {
-    console.log('  ' + item + ': ' + JSON.stringify(user[item]));
+const dumpUser = function (user) {
+  console.log('Dumping user:')
+  for (const item in user) {
+    console.log('  ' + item + ': ' + JSON.stringify(user[item]))
   }
-};
+}
 
-redmine.users({status: 1}, function(err, data) {
-  if (err) throw err;
+redmine
+  .users({ status: 1 })
+  .then(response => {
+    for (const i in response.data.issues) {
+      dumpUser(response.data.issues[i])
+    }
+    console.log('total_count: ', response.data.total_count)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
-  console.log(data);
+redmine
+  .get_user_by_id(9, { include: 'memberships,groups' })
+  .then(response => {
+    dumpUser(response.data.user)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
-  for (var i in data.users) {
-    dump_user(data.users[i]);
-  }
-
-  console.log('total_count: ' + data.total_count);
-});
-
-
-redmine.get_user_by_id(9, {include: "memberships,groups"}, function(err, data) {
-  if (err) throw err;
-
-
-  dump_user(data.user);
-});
-
-redmine.current_user({include: "memberships,groups"}, function(err, data) {
-  if (err) throw err;
-
-  dump_user(data.user);
-});
+redmine
+  .current_user({ include: 'memberships,groups' })
+  .then(response => {
+    dumpUser(response.data.user)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 /*
 var user = {
