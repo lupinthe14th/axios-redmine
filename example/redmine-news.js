c8040bd72c3e8c4ec3565c004ffe97fee42fbac7
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2018-10-23 22:11:48 +0900

    [modify] tweak

--- example/redmine-news.js
+++ example/redmine-news.js
@@ -1,41 +1,46 @@
 /*
- * get-redmine-issue - test request for issues
- * Author: wayne <wayne@zanran.me>
+ * Author: lupinthe14th <hideosuzuki@ordinarius-fectum.net>
  */
 
-'use strict()';
+'use strict()'
 
-var Redmine = require('../lib/redmine');
+const Redmine = require('../lib/redmine')
 
-///////////////////////////////////////////////////////////////
-var hostname = process.env.REDMINE_HOST || 'redmine.zanran.me';
-var config = {
-  apiKey: process.env.REDMINE_APIKEY || 'bed1ba0544b681e530c2447341607f423c9c8781',
-  format: 'json'
-};
+/// ////////////////////////////////////////////////////////////
+const hostname =
+  process.env.REDMINE_HOST || 'https://docker.for.mac.host.internal'
+const config = {
+  apiKey:
+    process.env.REDMINE_APIKEY || 'b7ce4d8d3865e79a75da8dba39bc801c12e36488',
+  rejectUnauthorized: process.env.REJECT_UNAUTHORIZED
+}
 
-var redmine = new Redmine(hostname, config);
+const redmine = new Redmine(hostname, config)
 
-
-var dump_news = function(fields) {
-  for (var item in fields) {
-    console.log('  ' + item + ': ' + JSON.stringify(fields[item]));
+const dumpNews = fields => {
+  for (const item in fields) {
+    console.log('  ' + item + ': ' + JSON.stringify(fields[item]))
   }
-};
+}
 
-redmine.news(function(err, data) {
-  if (err) throw err;
+redmine
+  .news()
+  .then(response => {
+    console.log(response.data)
+    for (const i in response.data.news) {
+      dumpNews(response.data.news[i])
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
-  console.log(data);
-
-  for (var i in data.news) {
-    dump_news(data.news[i]);
-  }
-});
-
-
-redmine.new_by_project_id(3, function(err, data) {
-  if (err) throw err;
-
-  dump_news(data.news);
-});
+redmine
+  .new_by_project_id(2)
+  .then(response => {
+    console.log(response.data)
+    dumpNews(response.data.news)
+  })
+  .catch(err => {
+    console.log(err)
+  })
