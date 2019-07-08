0519bcaf47ca1e1384613875cc94588e673846cc
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-07-09 01:07:39 +0900

    :bug: enable to work

--- test/issue.test.js
+++ test/issue.test.js
@@ -18,11 +18,12 @@ const redmine = new Redmine(hostname, config)
 
 describe('issue.test.js', function () {
   it('test-get-issue-with-invalid-id', function (done) {
+    const params = { include: 'attachments,journals,watchers' }
     try {
-      redmine.get_issue_by_id('0')
+      redmine.get_issue_by_id('0', params)
     } catch (e) {
       assert.strictEqual(
-        e.toString().toString(),
+        e.toString(),
         'Error: Issue ID must be an integer above 0 !'
       )
     }
