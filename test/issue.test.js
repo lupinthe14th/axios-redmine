11f82cf9274f8b789f4be5e3c99d28fb3e6256e0
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-10-28 22:17:16 +0900

    feat(test): add testcase

--- test/issue.test.js
+++ test/issue.test.js
@@ -6,6 +6,7 @@
 
 const assert = require('assert')
 const Redmine = require('../lib/redmine')
+const MockAdapter = require('axios-mock-adapter')
 
 /// ////////////////////////////////////////////////////////////
 const hostname = process.env.REDMINE_HOST || 'https://redmine.example.org'
@@ -15,6 +16,16 @@ const config = {
 }
 
 const redmine = new Redmine(hostname, config)
+
+const mock = new MockAdapter(redmine.instance)
+
+mock.onGet('/issues/1.json').reply(200, {
+  issue: {
+    project_id: 1,
+    subject: 'Example',
+    priority_id: 4
+  }
+})
 
 describe('issue.test.js', function () {
   it('test-get-issue-with-invalid-id', function (done) {
@@ -27,6 +38,15 @@ describe('issue.test.js', function () {
         'Error: Issue ID must be an integer above 0 !'
       )
     }
+    done()
+  })
+  it('test-get-issue-by-id-1', function (done) {
+    redmine.get_issue_by_id(1, {}).then(function (response) {
+      assert.strictEqual(response.status, 200)
+      assert.strictEqual(response.data.issue.project_id, 1)
+      assert.strictEqual(response.data.issue.subject, 'Example')
+      assert.strictEqual(response.data.issue.priority_id, 4)
+    })
     done()
   })
 })
