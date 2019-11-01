bac13ce654539f8b4d1185704b99e3b3b85e35f8
lupinthe14th <hideosuzuki@ordinarius-fectum.net>
2019-11-01 16:42:39 +0900

    fix(test): refactor test case

--- test/issue.test.js
+++ test/issue.test.js
@@ -17,10 +17,28 @@ const config = {
 
 const redmine = new Redmine(hostname, config)
 
-const mock = new MockAdapter(redmine.instance)
-
-mock.onGet('/issues/1.json').reply(200, {
-  issue: {
+const issues = [
+  {
+    project: { id: 1, name: 'Example Project' },
+    tracker: { id: 1, name: 'bug' },
+    status: { id: 1, name: 'new' },
+    priority: { id: 2, name: 'nomal' },
+    author: { id: 1, name: 'Redmine Admin' },
+    subject: 'Example',
+    description: '',
+    start_date: '2019-10-29',
+    due_date: null,
+    done_ratio: 0,
+    is_private: false,
+    estimated_hours: null,
+    total_estimated_hours: null,
+    spent_hours: 0,
+    total_spent_hours: 0,
+    created_on: '2019-10-29T13:50:28Z',
+    updated_on: '2019-10-29T13:50:28Z',
+    closed_on: null
+  },
+  {
     project: { id: 1, name: 'Example Project' },
     tracker: { id: 1, name: 'bug' },
     status: { id: 1, name: 'new' },
@@ -40,7 +58,8 @@ mock.onGet('/issues/1.json').reply(200, {
     updated_on: '2019-10-29T13:50:28Z',
     closed_on: null
   }
-})
+]
+const mock = new MockAdapter(redmine.instance)
 
 describe('issue.test.js', function () {
   it('test-get-issue-with-invalid-id', function (done) {
@@ -56,10 +75,69 @@ describe('issue.test.js', function () {
     done()
   })
   it('test-get-issue-by-id-1', async function () {
+    mock.onGet('/issues/1.json').reply(200, { issue: issues[0] })
     const response = await redmine.get_issue_by_id(1, {})
     assert.strictEqual(response.status, 200)
-    assert.strictEqual(response.data.issue.project.id, 1)
-    assert.strictEqual(response.data.issue.subject, 'Example')
-    assert.strictEqual(response.data.issue.priority.id, 2)
+    assert.deepStrictEqual(response.data.issue, issues[0])
+  })
+  it('test-listing-issues', async function () {
+    mock.onGet('/issues.json').reply(200, { issues: issues })
+    const response = await redmine.issues({})
+    assert.strictEqual(response.status, 200)
+    assert.deepStrictEqual(response.data.issues, issues)
+  })
+  it('test-creating-an-issue', async function () {
+    mock.onPost('/issues.json').reply(201)
+    const response = await redmine.create_issue({
+      issue: {
+        project_id: 1,
+        subject: 'Example',
+        priority_id: 4
+      }
+    })
+    assert.strictEqual(response.status, 201)
+  })
+  it('test-updating-an-issue', async function () {
+    mock
+      .onPut('/issues/1.json', {
+        issue: {
+          subject: 'Subject changed',
+          notes: 'The subject was changed'
+        }
+      })
+      .reply(200)
+    const response = await redmine.update_issue(1, {
+      issue: {
+        subject: 'Subject changed',
+        notes: 'The subject was changed'
+      }
+    })
+    assert.strictEqual(response.status, 200)
+  })
+  it('test-adding-a-watcher-with-invalid-user_id', function (done) {
+    try {
+      redmine.add_watcher(1, 1)
+    } catch (e) {
+      assert.strictEqual(
+        e.toString(),
+        'Error: user_id (required): id of the user to add as a watcher !'
+      )
+    }
+    done()
+  })
+  it('test-adding-a-watcher', async function () {
+    mock.onPost('/issues/1/watchers.json').reply(200, { user_id: 1 })
+    const response = await redmine.add_watcher(1, { user_id: 1 })
+    assert.strictEqual(response.status, 200)
+  })
+  it('test-removing-a-watcher', async function () {
+    mock.onDelete('/issues/1/watchers/1.json').reply(200)
+    const response = await redmine.remove_watcher(1, 1)
+    assert.strictEqual(response.status, 200)
+  })
+  it('test-deleting-an-issue', async function () {
+    mock.onDelete('/issues/1.json').reply(200)
+    const response = await redmine.delete_issue(1)
+    assert.strictEqual(response.status, 200)
   })
 })
